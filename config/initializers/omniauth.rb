27878e07d31724450001f3617c8077187473a7a8
Nick <nick@nick-desktop.(none)>
2012-11-01 21:34:45 -0500

    Fixing omniauth to work with  well anything :

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, 'SdYbGoqDQt0vHXpKZJstDA', '9bcuiJCTJ7CB1Rjjber56MgUF3EI5nXCnZmSPEqqvU'
+  provider :twitter, 'SdYbGoqDQt0vHXpKZJstDA', '9bcuiJCTJ7CB1Rjjber56MgUF3EI5nXCnZmSPEqqvU'
   #provider :identity, on_failed_registration: lambda { |env|
   #  IdentitiesController.action(:new).call(env)
   #}
