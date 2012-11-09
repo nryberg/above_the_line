3004ddeeda54e701e80f78b5fa6c90bea59baa12
Nick <nick@nick-desktop.(none)>
2012-11-08 20:33:40 -0600

    Implements initial radio button for direction

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item
 
   key :name, String
   key :value, Float
-  key :item_type, String # in or out
+  key :direction, String # in or out
   key :next_date, String #next day this can be paid
 end
